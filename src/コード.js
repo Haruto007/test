d16f1b6d997cece88ccb7fc41f06984c5cee36db
鈴木温人 <suzukiharuto@Hs-Macbook.local>
2026-04-18 19:13:37 +0900

    Initial commit

---
+++ src/コード.js
@@ -0,0 +1,21 @@
+// GitHub Actions 動作確認
+function myFunction() {
+  Logger.log("Hello, GAS! (Logger)");
+  console.log("Hello, GAS! (console)");
+}
+
+/**
+ * 動作確認用のテスト関数
+ */
+function testOutput() {
+  const now = new Date();
+  const message = "テスト実行時刻: " + now.toLocaleString();
+
+  Logger.log("--- Logger による出力 ---");
+  Logger.log(message);
+
+  console.log("--- console による出力 ---");
+  console.log(message);
+
+  return message;
+}
